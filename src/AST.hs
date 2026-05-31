60acb2cf09df1fb897ba021b0cded6707950e55e
Thierry-git <thierry.poulin000@gmail.com>
2026-05-31 13:52:27 -0400

    feat: AST data types

---
+++ src/AST.hs
@@ -0,0 +1,53 @@
+module AST
+  ( BetzaExpr,
+    Direction,
+    Behaviour,
+  )
+where
+
+data BetzaExpr = BetzaExpr ChainExpr
+  deriving (Show)
+
+data ChainExpr = ChainExpr OptionExpr (Maybe (ChainOperator, ChainExpr))
+  deriving (Show)
+
+data OptionExpr = Choose BetzaExpr | IffUnblocked BetzaExpr | Mandatory UnionExpr
+  deriving (Show)
+
+data UnionExpr = UnionExpr SetupExpr (Maybe UnionExpr)
+  deriving (Show)
+
+data SetupExpr = Setup ModifierExpr | NoSetup ModifierExpr
+  deriving (Show)
+
+data ModifierExpr = ModifierExpr [Modifier] ExponentExpr
+  deriving (Show)
+
+data ExponentExpr = ExponentExpr AtomExpr (Maybe Exponent)
+  deriving (Show)
+
+data AtomExpr = Paren BetzaExpr | From Label
+  deriving (Show)
+
+data ChainOperator = Step | Sequence
+  deriving (Show)
+
+data Modifier = Directional DirectionModifier | Behavioural Behaviour
+  deriving (Show)
+
+data DirectionModifier = Amalgamated Direction Direction | Single Direction
+  deriving (Show)
+
+data Direction = Forward | Backward | Leftward | Rightward | Sideway | Vertically | Any
+  deriving (Show)
+
+data Behaviour = Capture | Leap | Initial | Jump | Move | NoJump | Hop | All
+  deriving (Show)
+
+data Exponent = Infinite | Repeat Number | ModifiedRepeat (Maybe ChainOperator) [Modifier]
+  deriving (Show)
+
+type Number = Int
+
+data Label = Upper Char | Descriptor String
+  deriving (Show)
