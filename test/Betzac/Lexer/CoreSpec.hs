6e5ef46da3510bf54e6f0e01fa7becea204a3244
Thierry-git <thierry.poulin000@gmail.com>
2026-06-01 00:37:49 -0400

    test(lexer): implemented more core tests (sat, char)

--- test/Betzac/Lexer/CoreSpec.hs
+++ test/Betzac/Lexer/CoreSpec.hs
@@ -16,3 +16,19 @@ spec = do
                 runLexer peek "abc" `shouldBe` Right (Just 'a', "abc")
             it "returns Nothing on empty input" $
                 runLexer peek "" `shouldBe` Right (Nothing, "")
+
+        describe "sat" $ do
+            context "when the predicate is satisfied" $
+                it "consumes and returns the first character" $ do
+                    runLexer (sat $ \c -> c `elem` ['A' .. 'Z']) "Haskell" `shouldBe` Right ('H', "askell")
+                    runLexer (sat $ const True) "anything!" `shouldBe` Right ('a', "nything!")
+            context "when the predicate is not satisfied" $
+                it "returns a LexError" $ do
+                    runLexer (sat $ \c -> c `notElem` ['A' .. 'Z']) "Haskell" `shouldBe` Left LexError
+                    runLexer (sat $ const False) "nothing..." `shouldBe` Left LexError
+
+        describe "char" $ do
+            it "consumes and returns the first character if the next char matches" $
+                runLexer (char 'b') "betzac" `shouldBe` Right ('b', "etzac")
+            it "returns a LexError if the next char does not match" $
+                runLexer (char '_') "betzac" `shouldBe` Left LexError
