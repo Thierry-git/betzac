e3fb6ffae6516862daf1dba7cf39c206c6ac8421
Thierry-git <thierry.poulin000@gmail.com>
2026-06-07 06:51:50 -0400

    feat(betzac): made a little interactive lexer for interactive testing

--- app/Betzac/Main.hs
+++ app/Betzac/Main.hs
@@ -1,9 +1,18 @@
 module Main (main) where
 
-import Betzac.Lexer.Expr (lexExpr, runLexer)
+import Betzac.Lexer.Core (LexError (..))
+import Betzac.Pipeline (lexSource)
+import Control.Monad
+import System.IO
 
 main :: IO ()
 main = do
-    putStrLn "Om nom nom! Feed me stuff to lex (Ctrl+D to finish):"
-    s <- getContents
-    putStrLn . show $ runLexer lexExpr s
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stdin NoBuffering
+    putStrLn "Type a betza expression:"
+    forever $ do
+        putStr "> "
+        line <- getLine
+        case lexSource line of
+            Left (LexError pos) -> putStrLn $ "Error at position " ++ show pos
+            Right tokens -> mapM_ print tokens
